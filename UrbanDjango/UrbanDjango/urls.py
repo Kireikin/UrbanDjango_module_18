381472bb12ab2912c01532975959aafea7cfabce
Kireikin <kireikinav@yandex.ru>
2025-01-09 22:02:32 +0300

    09/01/2025_for_module_18_2_hw

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from UrbanDjango.task2.views import func_template, ClassTemplate
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('func/', func_template),
+    path('class/', ClassTemplate.as_view()),
+    path('', TemplateView.as_view(temlate_name='welcome.html')),
 ]
